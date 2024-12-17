11830c5ab81369f583dcb64b852d402be142c752
Het Patel <hpatel31@terpmail.umd.edu>
2024-12-17 16:29:35 -0500

    created a function

--- suspension_travel.py
+++ suspension_travel.py
@@ -14,24 +14,25 @@ import argparse
 import matplotlib.pyplot as plt
 
 # define arguments
-
+# change this part
 parser = argparse.ArgumentParser(description="View RaceCapture suspension travel data")
 parser.add_argument('filename')
 args = parser.parse_args()
 
-# read data csv, extract travel data, and then analyze useful values
-# you risk losing data by dropping naively
+# Reads the data csv file, extract travel data, and then analyzes useful values
 df = pd.read_csv(args.filename)
 
-# Converting large 
+# Converting large and weird named headings into more manageable names.
 df = df.rename(columns= {'Interval|"ms"|0|0|1': "Interval", 
                         'Analog1|"Volts"|0.0|5.0|100': "Front left suspension", 
                         'Analog2|"Volts"|0.0|5.0|100': "Front right suspension"})
 
 # All implementation of the equations in the data.
-df['Distance_left'] = 9.36 * df['Front left suspension'] - 12.2 # front left suspension.
-df['Distance_right'] = 9.92 * df['Front right suspension'] - 14.2 # front right suspension.
-df['Time (seconds)'] = df['Interval'] / 1000 # interval in ms is converted in seconds.
+def calculate_travel_dist_time(df):
+    # make a function for this part
+    df['Distance_left'] = 9.36 * df['Front left suspension'] - 12.2 # front left suspension.
+    df['Distance_right'] = 9.92 * df['Front right suspension'] - 14.2 # front right suspension.
+    df['Time (seconds)'] = df['Interval'] / 1000 # interval in ms is converted in seconds.
 
 # plots the data
 df.plot.scatter(x= 'Time (seconds)', y='Distance_right', color = 'red')
